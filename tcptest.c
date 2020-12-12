33d4d072f191291e1922065e838032fd57c332af
Anders Trier <anders.trier.olesen@gmail.com>
2020-12-12 17:05:14 +0100

    Now usefull for testing when your TCP connections are killed

--- tcptest.c
+++ tcptest.c
@@ -23,19 +23,23 @@ int do_connect(struct sockaddr_in *dst, int keepalive_time) {
         return -1;
     }
 
+    // Enable TCP keepalive
     if (setsockopt(s, SOL_SOCKET, SO_KEEPALIVE,
                 (int []){1}, sizeof(int)) == -1) {
         perror("setsockopt SO_KEEPALIVE");
         return -1;
     }
 
+    // The time (in seconds) the connection needs to remain idle before
+    // TCP starts sending keepalive probes, if the socket option
+    // SO_KEEPALIVE has been set on this socket.
     if (setsockopt(s, IPPROTO_TCP, TCP_KEEPIDLE,
                 &keepalive_time, sizeof keepalive_time) == -1) {
         perror("setsockopt SO_KEEPALIVE");
         return -1;
     }
 
-
+/* For faster debugging
     // The maximum number of keepalive probes TCP  should  send  before
     // dropping the connection.
     if (setsockopt(s, IPPROTO_TCP, TCP_KEEPCNT,
@@ -50,13 +54,15 @@ int do_connect(struct sockaddr_in *dst, int keepalive_time) {
         perror("setsockopt TCP_KEEPINTVL");
         return -1;
     }
+*/
 
     return s;
 }
 
-#define NCONNECTIONS 4
+#define NCONNECTIONS 130
 int main(int argc, char *argv[]){
     int tcpsessions[NCONNECTIONS] = {0};
+    int keepalivetime[NCONNECTIONS] = {0};
     char buf;
     int ret;
     struct sockaddr_in dst_addr = {
@@ -65,23 +71,26 @@ int main(int argc, char *argv[]){
         .sin_addr   = inet_addr("130.225.254.98"),
     };
 
-    struct timeval tv = {0};
-
-
+    printf("[+] Trying to establish connections: ");
     for (int i = 0; i < NCONNECTIONS; i++) {
-        tcpsessions[i] = do_connect(&dst_addr, (i+1) * 10);
+        fflush(stdout);
+        // The first connection will have a keepalivetime of 1 min,
+        // the second one 2 min, etc.
+        keepalivetime[i] = (i + 1) * 60;
+        printf("%d (%ds), ", i + 1, keepalivetime[i]);
+        tcpsessions[i] = do_connect(&dst_addr, keepalivetime[i]);
         if (tcpsessions[i] == -1) {
             return EXIT_FAILURE;
         }
     }
+    printf("\n[+] All connections established\n");
 
 
-
+    // select() loop
     for (;;) {
         fd_set rfds;
         FD_ZERO(&rfds);
-        tv.tv_sec = 30;
-        tv.tv_usec = 0;
+        int openconnections = 0;
         int maxfd = -1;
 
         for (int i = 0; i < NCONNECTIONS; i++) {
@@ -89,40 +98,47 @@ int main(int argc, char *argv[]){
                 continue;
             FD_SET(tcpsessions[i], &rfds);
             maxfd = tcpsessions[i] > maxfd ? tcpsessions[i] : maxfd;
+            openconnections++;
         }
-        if (maxfd == -1) {
-            printf("No more alive connections left\n");
+        if (openconnections == 0) {
+            printf("[+] No more alive connections left\n");
             return EXIT_SUCCESS;
         }
 
-        ret = select(maxfd + 1, &rfds, NULL,
-                NULL, /*&tv*/ NULL);
+        printf("[+] Waiting for a connection to timeout.\n");
+        printf("[+] Open connections: %d\n",
+                openconnections);
+        ret = select(maxfd + 1, &rfds, NULL, NULL, NULL);
         if (ret == -1) {
             if (errno == EINTR)
                 continue;
             perror("select");
+            return EXIT_FAILURE;
         } else if (ret) {
-            printf("Data\n");
             for (int i = 0; i < NCONNECTIONS; i++) {
                 if (FD_ISSET(tcpsessions[i], &rfds)) {
                     ssize_t readret = read(tcpsessions[i], &buf, 1);
-                    if (readret != -1 || errno != ETIMEDOUT){
-                        printf("shouldn't happen:\n"
+                    if (readret != -1 || errno != ETIMEDOUT) {
+                        printf("[-] shouldn't happen:\n"
                                 "readret: %ld\n"
-                                "errno: %d\n"
+                                "errno: %d (%m)\n"
                                 "i: %d\n"
                                 "tcpsessions[i]: %d\n",
                                 readret, errno, i, tcpsessions[i]
                               );
+                        close(tcpsessions[i]);
+                        tcpsessions[i] = -1;
+                        continue;
                     }
-                    printf("yay!\n");
-                    printf("read returned %ld for i = %d, s = %d\n",
-                            readret, i, tcpsessions[i]);
+                    printf("[+] TCP connection died: ");
+                    printf("Connection %d, keepalivetime: %dm %ds\n",
+                            i, keepalivetime[i]/60, keepalivetime[i]%60);
+                    close(tcpsessions[i]);
                     tcpsessions[i] = -1;
                 }
             }
         } else {
-            printf("No data\n");
+            printf("[+] No data\n");
         }
     }
 

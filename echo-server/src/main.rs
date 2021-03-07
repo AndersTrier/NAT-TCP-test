e281fbe355ffb1a264b6e55ed27c91ba504895a8
AndersTrier <anders.trier.olesen@gmail.com>
2021-03-07 21:53:10 +0100

    Fix 'listener.incoming() error: Too many open files (os error 24)' by setting RLIMIT_NOFILE to the maximum value currently allowed

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -2,6 +2,7 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::Read;
 use std::io::Write;
+use rlimit::*;
 
 fn handle_client(mut stream: TcpStream) {
     // Read up to 64 bytes from stream
@@ -20,7 +21,25 @@ fn handle_client(mut stream: TcpStream) {
     }
 }
 
+fn raise_rlimit_nofile() -> std::io::Result<()> {
+    // Allow this process to have many open connections (file descriptors)
+    // Check GETRLIMIT(2) - RLIMIT_NOFILE for more info
+    let (nofile_soft, nofile_hard) = getrlimit(Resource::NOFILE)?;
+    if nofile_soft != nofile_hard {
+        // GETRLIMIT(2):
+        // The hard limit acts  as  a  ceiling  for  the  soft
+        // limit:  an  unprivileged process may set only its soft limit to a value
+        // in the range from 0 up to the hard limit
+        return setrlimit(Resource::NOFILE, nofile_hard, nofile_hard);
+    }
+    Ok(())
+}
+
 fn main() {
+    raise_rlimit_nofile().unwrap_or_else(|error| {
+        println!("Falied to update RLIMIT_NOFILE: {:?}. Continuing.", error);
+    });
+
     let listener = TcpListener::bind("0.0.0.0:31415").unwrap();
 
     for stream in listener.incoming() {
